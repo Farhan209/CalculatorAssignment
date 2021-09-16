66c73c60f8beaa633caecb35d5d10024e3088f84
Farhan209 <farhan209@hotmail.com>
2021-09-16 15:38:46 +0100

    database implementation

---
+++ CalculatorWinForm/DatabaseConnection.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatorWinForm
+{
+    class DatabaseConnection
+    {
+
+        public static void insertData(int firstNum, int secondNum, string arithmetic, int outputR)
+        {
+            string connectionString = @"Server=FARHAN\SQLEXPRESS;Database=CalculatorApp;User ID=sa;Password=Dmc208209";
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                SqlCommand insertValues = new SqlCommand("Insert into Results(first_number, second_number, arithmetic, result) values(@firstValue, @secondValue, @operatorA, @outputRes)", connection);
+                insertValues.Parameters.AddWithValue("@firstValue", firstNum);
+                insertValues.Parameters.AddWithValue("@secondValue", secondNum);
+                insertValues.Parameters.AddWithValue("@operatorA", arithmetic);
+                insertValues.Parameters.AddWithValue("@outputRes", outputR);
+                connection.Open();
+                insertValues.ExecuteNonQuery();
+                connection.Close();
+            }
+        }
+    }
+}
--- CalculatorWinForm/Form1.cs
+++ CalculatorWinForm/Form1.cs
@@ -23,6 +23,8 @@ namespace CalculatorWinForm
             int secondNumber = Convert.ToInt32(secondInput.Text);
             var resultNumber = HttpArithmetics.getCalculatorAdd(firstNumber, secondNumber);
             resultOutput.Text = resultNumber.ToString();
+            var operatorA = "Add";
+            DatabaseConnection.insertData(firstNumber, secondNumber, operatorA, resultNumber);
         }
 
         private void subButton_Click(object sender, EventArgs e)
@@ -31,6 +33,8 @@ namespace CalculatorWinForm
             int secondNumber = Convert.ToInt32(secondInput.Text);
             var resultNumber = HttpArithmetics.getCalculatorSub(firstNumber, secondNumber);
             resultOutput.Text = resultNumber.ToString();
+            var operatorA = "Sub";
+            DatabaseConnection.insertData(firstNumber, secondNumber, operatorA, resultNumber);
         }
 
         private void multButton_Click(object sender, EventArgs e)
@@ -39,6 +43,8 @@ namespace CalculatorWinForm
             int secondNumber = Convert.ToInt32(secondInput.Text);
             var resultNumber = HttpArithmetics.getCalculatorMult(firstNumber, secondNumber);
             resultOutput.Text = resultNumber.ToString();
+            var operatorA = "Mult";
+            DatabaseConnection.insertData(firstNumber, secondNumber, operatorA, resultNumber);
         }
 
         private void divButton_Click(object sender, EventArgs e)
@@ -47,6 +53,8 @@ namespace CalculatorWinForm
             int secondNumber = Convert.ToInt32(secondInput.Text);
             var resultNumber = HttpArithmetics.getCalculatorDiv(firstNumber, secondNumber);
             resultOutput.Text = resultNumber.ToString();
+            var operatorA = "Div";
+            DatabaseConnection.insertData(firstNumber, secondNumber, operatorA, resultNumber);
         }
     }
 }
